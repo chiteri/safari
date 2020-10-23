e07fc6390b6a93e4351ec5c032cbbd3184c75520
Martin Chiteri <martin.chiteri@gmail.com>
2020-10-22 20:48:03 -0600

    Alter page layout and add navigation as part of header. Also make navigation menu responsive

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -4,36 +4,39 @@ import Aux from '../../hoc/Auxilliary';
 
 const layout = (props) => (
     <Aux>
-        <header><h1>Safari</h1></header>
-        <nav>
-            <ul>
-                <li>Dash</li>
-                <li>Trip</li>
-                <li>Profile</li>
-            </ul>
-            <section>
-            <div>
-                <label for="from"></label>
-                <input type="text" id="from" placeholder="From"></input>
-            </div>
+        <header>
+            <h1>Safiri</h1>
+            <nav>
+                <ul>
+                    <li><a href="#">Dash</a></li>
+                    <li><a href="#">Trips</a></li>
+                    <li><a href="#">Profile</a></li>
+                    <li><a href="#">Logout</a></li>
+                </ul>
+            </nav>
+        </header>            
+        <section>
+                <div>
+                    <label for="from"></label>
+                    <input type="text" id="from" placeholder="From"></input>
+                </div>
 
-            <div>
-                <label for="to"></label>
-                <input type="text" id="to" placeholder="Destination"></input>
-            </div>
+                <div>
+                    <label for="to"></label>
+                    <input type="text" id="to" placeholder="Destination"></input>
+                </div>
 
-            <div>
-                <button id="request">Request</button>
-                <button id="cancel">Cancel</button>
-            </div>
+                <div>
+                    <button id="request">Request</button>
+                    <button id="cancel">Cancel</button>
+                </div>
             </section>
-            
-        </nav>
-        
         <main>
             {props.children}
         </main>
-        <aside>ASIDE</aside>
+        <aside>
+            <p>ASIDE</p>
+        </aside>
         <footer> 
             <hr /> All rights reserved, MMXX&copy; </footer>
     </Aux>
