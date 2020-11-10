29b86c795deaca7183e59b6bb2c0bbdc33edd919
Martin Chiteri <martin.chiteri@gmail.com>
2020-11-09 20:41:52 -0700

    Update map component class code with relevant comments

--- src/components/RideMap/RideMap.js
+++ src/components/RideMap/RideMap.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
+import PropTypes from 'prop-types';
 import styles from './RideMap.module.css';
 
 class RideMap extends Component {
@@ -16,15 +17,19 @@ class RideMap extends Component {
         };
     }
 
+    // STEP 1) Happens when the map is first loaded
     componentDidUpdate(prevProps, prevState) {
-        if (prevProps.google !== this.props.google) {
-          this.loadMap();
-        }
-        if (prevState.currentLocation !== this.state.currentLocation) {
-          this.recenterMap();
-        }
+      // Check whether the map API is available
+      if (prevProps.google !== this.props.google) {
+        this.loadMap(); // Let the map update
+      }
+      // Check for an update to the currentLocation in the state after the component itself is updated
+      if (prevState.currentLocation !== this.state.currentLocation) {
+        this.recenterMap();
+      }
     }
 
+    // Function called when the currentLocation in the map component's state is updated
     recenterMap() {
         const map = this.map;
         const current = this.state.currentLocation;
@@ -37,68 +42,79 @@ class RideMap extends Component {
         }
     }
 
+    // STEP 2) Case when the map is available when component mounts
     componentDidMount() {
-        if (this.props.centerAroundCurrentLocation) {
-          if (navigator && navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(pos => {
-              const coords = pos.coords;
-              this.setState({
-                currentLocation: {
-                  lat: coords.latitude,
-                  lng: coords.longitude
-                }
-              });
+      // The map has previously been loaded in our app
+      if (this.props.centerAroundCurrentLocation) {
+        // Use the current location of the user 
+        if (navigator && navigator.geolocation) {
+          // Fetch the current position
+          navigator.geolocation.getCurrentPosition(pos => {
+            // Update the state of our map component using this position object
+            const coords = pos.coords;
+            this.setState({
+              currentLocation: {
+                lat: coords.latitude,
+                lng: coords.longitude
+              }
             });
-          }
-        }
-        this.loadMap();
-    }
-
-    loadMap() {
-        if (this.props && this.props.google) {
-          // checks if google is available
-          const { google } = this.props;
-          const maps = google.maps;
-    
-          const mapRef = this.refs.map;
-    
-          // reference to the actual DOM element
-          const node = ReactDOM.findDOMNode(mapRef);
-    
-          let { zoom } = this.props;
-          const { lat, lng } = this.state.currentLocation;
-          const center = new maps.LatLng(lat, lng);
-    
-          const mapConfig = Object.assign(
-            {},
-            {
-              center: center,
-              zoom: zoom, 
-              style: styles.Map
-            }
-          );
-    
-          // maps.Map() is constructor that instantiates the map
-          this.map = new maps.Map(node, mapConfig);
-        }
-    }
-
-    renderChildren() {
-        const { children } = this.props;
-    
-        if (!children) return;
-    
-        return React.Children.map(children, c => {
-          if (!c) return;
-    
-          return React.cloneElement(c, {
-            map: this.map,
-            google: this.props.google,
-            mapCenter: this.state.currentLocation
           });
-        });
+        }
+      }
+      this.loadMap();
     }
 
+    // Function to get any of our maps on the page
+    // This function is only called after the component has been
+    // rendered (i.e there is a DOM component on the page)
+    loadMap() {
+      // checks if google API is available
+      if (this.props && this.props.google) {
+        const { google } = this.props;
+        const maps = google.maps;
+  
+        const mapRef = this.refs.map;
+  
+        // Obtain a reference to the actual DOM element to place map in
+        const node = ReactDOM.findDOMNode(mapRef); // (This is not a virtual DOM element)
+  
+        let { zoom } = this.props;
+        const { lat, lng } = this.state.currentLocation;
+        const center = new maps.LatLng(lat, lng);
+  
+        const mapConfig = Object.assign(
+          {},
+          {
+            center: center,
+            zoom: zoom, // How close to the center we should display
+            style: styles.Map
+          }
+        );
+  
+        // maps.Map() constructor accepts a DOM node and a configurator object to create map
+        this.map = new maps.Map(node, mapConfig);
+      }
+    }
+
+    // Allow the component to also call lifecycle methods of its children  
+    renderChildren() {
+      const { children } = this.props;
+  
+      if (!children) return; // No children have been passed to map instance
+  
+      return React.Children.map(children, c => {
+        if (!c) return;
+        
+        // Create clones of children to display on map
+        return React.cloneElement(c, {
+          map: this.map,
+          google: this.props.google,
+          mapCenter: this.state.currentLocation
+        });
+      });
+    }
+
+    // Allow the component to place map on page
     render() {
       const style = Object.assign({}, styles.Map);
     
@@ -112,8 +128,18 @@ class RideMap extends Component {
 
 }
 
+// Define some default properties in case they are not passed
+RideMap.propTypes = {
+  google: PropTypes.object,
+  zoom: PropTypes.number,
+  initialCenter: PropTypes.object, 
+  centerAroundCurrentLocation: PropTypes.bool,
+  visible: PropTypes.bool
+};
+
 RideMap.defaultProps = {
-    zoom: 14,
+    zoom: 10,
+    // Edmonton by default
     initialCenter: {
       lat: 53.5461,
       lng: 113.4938
